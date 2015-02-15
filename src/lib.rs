876f454497f448506fb60888cb0b660f4f908635
J. Aaron Gustafson <jag426@cornell.edu>
2015-02-15 01:58:01 -0500

    switch from the weird fun double stack thing to a single Vec<u8> for the tape, which is approximately one billion times faster

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(io)]
 
-use std::collections::DList;
 use std::old_io::{Reader, Writer};
 
 pub struct Interpreter<R, W> where R: Reader, W: Writer {
@@ -10,25 +9,22 @@ pub struct Interpreter<R, W> where R: Reader, W: Writer {
     // the source of input and destination of output for the program
     input: R,
     output: W,
-    // an infinite array of bytes implemented as two stacks. current stores the
-    // current cell's value.
-    // e.g. movement to the left means pushing current onto stack2 and popping
-    // from stack1 onto current
-    stack1: DList<u8>,
-    current: u8,
-    stack2: DList<u8>,
+    // the array of byte cells
+    tape: Vec<u8>,
+    pos: usize,
 }
 
 impl<R, W> Interpreter<R, W> where R: Reader, W: Writer {
     pub fn new(program: Vec<u8>, input: R, output: W) -> Self {
+        let mut tape = Vec::with_capacity(100us);
+        tape.push(0u8);
         Interpreter {
             program: program,
             pc: 0us,
             input: input,
             output: output,
-            stack1: DList::new(),
-            current: 0u8,
-            stack2: DList::new(),
+            tape: tape,
+            pos: 0us,
         }
     }
 
@@ -40,48 +36,43 @@ impl<R, W> Interpreter<R, W> where R: Reader, W: Writer {
         match self.program[self.pc] {
             62u8 => {
                 // > 
-                self.stack1.push_front(self.current);
-                self.current = match self.stack2.pop_front() {
-                    Some(c) => c,
-                    None => 0u8,
-                };
+                self.pos += 1us;
+                if self.pos == self.tape.len() {
+                    self.tape.push(0u8);
+                }
                 self.pc += 1us;
             }
             60u8 => {
                 // <
-                // TODO noop if the pointer is all the way to the left already
-                match self.stack1.pop_front() {
-                    Some(c) => {
-                        self.stack2.push_front(self.current);
-                        self.current = c;
-                    }
-                    None => {}
+                // noop if the pointer is all the way to the left already
+                if self.pos != 0us {
+                    self.pos -= 1us;
                 }
                 self.pc += 1us;
             }
             43u8 => {
                 // +
-                self.current += 1u8;
+                self.tape[self.pos] += 1u8;
                 self.pc += 1us;
             }
             45u8 => {
                 // -
-                self.current -= 1u8;
+                self.tape[self.pos] -= 1u8;
                 self.pc += 1us;
             }
             46u8 => {
                 // .
-                self.output.write_u8(self.current).unwrap();
+                self.output.write_u8(self.tape[self.pos]).unwrap();
                 self.pc += 1us;
             }
             44u8 => {
                 // ,
-                self.current = self.input.read_u8().unwrap();
+                self.tape[self.pos] = self.input.read_u8().unwrap();
                 self.pc += 1us;
             }
             91u8 => {
                 // [
-                if self.current == 0u8 {
+                if self.tape[self.pos] == 0u8 {
                     let mut open_brackets = 1us;
                     while open_brackets > 0us {
                         self.pc += 1us;
@@ -99,7 +90,7 @@ impl<R, W> Interpreter<R, W> where R: Reader, W: Writer {
             }
             93u8 => {
                 // ]
-                if self.current != 0u8 {
+                if self.tape[self.pos] != 0u8 {
                     let mut open_brackets = 1us;
                     while open_brackets > 0us {
                         if self.pc == 0us {
